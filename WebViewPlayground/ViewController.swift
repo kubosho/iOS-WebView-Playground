789665b1f3f64116d0392fd6d41bc873200f6439
Shota Kubota <ta2@o2p.jp>
2022-07-20 19:26:45 +0900

    Initial Commit

---
+++ WebViewPlayground/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebViewPlayground
+//
+//  Created by 久保田 翔太 on 2022/07/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
